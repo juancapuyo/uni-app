d956cae0ada8d6d6cad13d16a45d60380abbf2d8
Juan <juancapuyo7@gmail.com>
2024-07-08 09:47:08 +1000

    Bonus: Used UI package to style table. Custom sticky header.

--- src/components/UniTable.js
+++ src/components/UniTable.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './UniTable.css';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack'
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const UniTable = () => {
   const [universities, setUniversities] = useState([]);
@@ -28,33 +35,35 @@ const UniTable = () => {
 
   return (
     <div className="uni-table-container">
-      <div className="buttons">
+    <div className='buttons'>
       <Stack direction="row" spacing={2} justifyContent="center">
         <Button variant="contained" onClick={loadUniversities}>LOAD</Button>
         <Button variant="contained" color="error" onClick={deleteLastUniversity}>DELETE</Button>
         <Button  variant="contained" color="success" onClick={addFirstUniversityToEnd}>ADD</Button>
         </Stack>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>State</th>
-            <th>Country</th>
-            <th>Website</th>
-          </tr>
-        </thead>
-        <tbody>
+    </div>
+    <TableContainer component={Paper} sx={{ maxHeight: 650 }}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+            <TableCell sx={{ backgroundColor: '#f4f4f4', border: '1px solid #ddd' }}>Name</TableCell>
+            <TableCell sx={{ backgroundColor: '#f4f4f4', border: '1px solid #ddd' }}>State</TableCell>
+            <TableCell sx={{ backgroundColor: '#f4f4f4', border: '1px solid #ddd' }}>Country</TableCell>
+            <TableCell sx={{ backgroundColor: '#f4f4f4', border: '1px solid #ddd' }}>Website</TableCell>
+            </TableRow>
+          </TableHead>
+        <TableBody>
           {universities.map((university, index) => (
-            <tr key={index}>
-              <td>{university.name}</td>
-              <td>{university['state-province'] || 'N/A'}</td>
-              <td>{university.country}</td>
-              <td><a href={university.web_pages[0]} target="_blank" rel="noopener noreferrer">{university.web_pages[0]}</a></td>
-            </tr>
+            <TableRow key={index}>
+              <TableCell sx={{ border: '1px solid #ddd'}}>{university.name}</TableCell>
+              <TableCell sx={{ border: '1px solid #ddd'}}>{university['state-province'] || 'N/A'}</TableCell>
+              <TableCell sx={{ border: '1px solid #ddd'}}>{university.country}</TableCell>
+              <TableCell sx={{ border: '1px solid #ddd'}}><a href={university.web_pages[0]} target="_blank" rel="noopener noreferrer">{university.web_pages[0]}</a></TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 };
